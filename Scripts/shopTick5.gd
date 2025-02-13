d8234062d4eb6b4cf1f3c2f6855302b45056c37a
NightGamer <mittal.rishabh181198@gmail.com>
2025-02-13 16:48:32 -0500

    Update

---
+++ Scripts/shopTick5.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body: PhysicsBody2D):
+	print("Wow")
+	entered = true
+	Global.tick = 5
+	print(Global.tick)
+	get_tree().change_scene_to_file("res://Scenes/world_2.tscn")
