33ebd79f0e9d7e996e9a22a2fb91b06fcac67982
NightGamer <mittal.rishabh181198@gmail.com>
2025-02-13 17:11:14 -0500

    Big Update

---
+++ Scripts/beach_world.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+var entered = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite_2d.visible = false
+
+func _process(delta: float) -> void:
+	if Global.tick >= 3:
+		sprite_2d.visible = true  # Show sprite when tick is 2
+	else:
+		sprite_2d.visible = false  # Hide sprite otherwise
+
+		
+func _on_body_entered(body: PhysicsBody2D):
+	if Global.tick >= 4:
+		print("Autumn World")
+		entered = true
+		get_tree().change_scene_to_file("res://Scenes/_FireAutumnWorld-RishabhMittal.tscn")
